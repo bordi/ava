5b376c075b8d6c34d2cb03b679230022f46cbb3a
Bordi Art <bordyanartem@gmail.com>
2017-05-14 16:28:07 +0300

    Fi retina loaded mage

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -42,13 +42,16 @@ $(window).on('load', function() {
     loadImage();
 });
 
+function isRetina(){
+    return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+}
+
 function loadImage() {
     var img = new Image();
 
-    img.src = "./img/bg-heropage.jpg";
+    img.src = isRetina() ? "./img/bg-heropage@2x.jpg" : "./img/bg-heropage.jpg";
 
     img.onload = function() {
         $('#page-hero').addClass('page-hero--loaded');
-        //$('#page-hero').css('background-image', 'url('+img.src+')');
     };
 }
