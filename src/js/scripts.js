a24537756118337550c40d5973362333f8499a92
Bordi Art <bordyanartem@gmail.com>
2017-05-14 16:41:18 +0300

    Added resize handler

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -42,11 +42,21 @@ $(window).on('load', function() {
     loadImage();
 });
 
-function isRetina(){
+$(window).resize(loadImage);
+
+function detectMob() {
+    return window.innerWidth <= 767;
+}
+
+function isRetina() {
     return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 }
 
 function loadImage() {
+    if(detectMob()) {
+        return;
+    }
+    
     var img = new Image();
 
     img.src = isRetina() ? "./img/bg-heropage@2x.jpg" : "./img/bg-heropage.jpg";
